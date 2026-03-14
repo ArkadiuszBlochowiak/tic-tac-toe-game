1138f8c4f047fb4c938c73a3c13cb35cee1c022e
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-03-14 14:48:46 +0100

    Extract utils

--- src/features/game/components/Board.tsx
+++ src/features/game/components/Board.tsx
@@ -1,5 +1,6 @@
 import "../styles/board.css";
-import type { SquareElement, WinnerSequence } from "../types/game.ts";
+import type { SquareElement } from "../types/game.ts";
+import { calculateWinner } from "../utils/calculateWinner.ts";
 
 function Square({
   value,
@@ -24,33 +25,6 @@ function Square({
       {value}
     </div>
   );
-}
-
-function calculateWinner(list: SquareElement[]): WinnerSequence | null {
-  const squares: SquareElement[] = list.slice();
-
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (const indices of lines) {
-    const [a, b, c] = indices;
-    if (
-      squares[a].value &&
-      squares[a].value === squares[b].value &&
-      squares[b].value === squares[c].value
-    ) {
-      return { type: squares[a].value, sequence: indices };
-    }
-  }
-  return null;
 }
 
 function Status({
---
+++ src/features/game/utils/calculateWinner.ts
@@ -0,0 +1,28 @@
+import type { SquareElement, WinnerSequence } from "../types/game.ts";
+
+export function calculateWinner(list: SquareElement[]): WinnerSequence | null {
+  const squares: SquareElement[] = list.slice();
+
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (const indices of lines) {
+    const [a, b, c] = indices;
+    if (
+      squares[a].value &&
+      squares[a].value === squares[b].value &&
+      squares[b].value === squares[c].value
+    ) {
+      return { type: squares[a].value, sequence: indices };
+    }
+  }
+  return null;
+}
