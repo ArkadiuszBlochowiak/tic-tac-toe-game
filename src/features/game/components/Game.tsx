8fe77ae85805c2276b72d90bad261e898f4b75e8
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-03-15 12:33:37 +0100

    Show action buttons after game end

--- src/features/game/components/Game.tsx
+++ src/features/game/components/Game.tsx
@@ -3,6 +3,32 @@ import "../styles/game.css";
 import Board from "./Board.tsx";
 import type { SquareElement } from "../types/game.ts";
 import Moves from "./Moves.tsx";
+import { Link } from "react-router";
+import { calculateWinner } from "../utils/calculateWinner.ts";
+
+function AfterGameActions({
+  squares,
+  resetState,
+}: {
+  squares: SquareElement[];
+  resetState: () => void;
+}) {
+  const allSelected = squares.every((square) => Boolean(square.value));
+  const shouldShowActions = Boolean(calculateWinner(squares)) || allSelected;
+
+  const actions = (
+    <div className="actions">
+      <button type="button" onClick={resetState}>
+        Play again
+      </button>
+      <Link to="/">
+        <button type="button">Return to home page</button>
+      </Link>
+    </div>
+  );
+
+  return <div>{shouldShowActions && actions}</div>;
+}
 
 export default function Game() {
   const list: SquareElement[] = [];
@@ -30,10 +56,21 @@ export default function Game() {
     setCross(step % 2 === 0);
   }
 
+  function handleReset(): void {
+    setCurrentMove(0);
+    setHistory([list]);
+    setCross(true);
+  }
+
   return (
-    <div className="game">
-      <Board squares={currentStep} isCross={isCross} onUpdate={handleMove} />
-      <Moves moves={history} onUpdate={handleCurrentStep} />
-    </div>
+    <>
+      <div className="game">
+        <Board squares={currentStep} isCross={isCross} onUpdate={handleMove} />
+        <Moves moves={history} onUpdate={handleCurrentStep} />
+      </div>
+      <div className="game-actions">
+        <AfterGameActions squares={currentStep} resetState={handleReset} />
+      </div>
+    </>
   );
 }
