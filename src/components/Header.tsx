983dfa6e22e454dc3e94919671720d963a29ab0f
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-03-15 14:49:23 +0100

    Add game logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import "./header.css";
 import { Link } from "react-router";
+import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
     <div className="header">
-      <div className="header__logo">place for logo</div>
+      <div className="header__logo">
+        <img className="logo" src={logo} alt="game logo" />
+      </div>
       <h2 className="header__title">
-        <Link to={"/"}> Tac Toe Game </Link>
+        <Link to={"/"}> Tic Tac Toe Game </Link>
       </h2>
       {/* place for dark/light mode switch */}
     </div>
